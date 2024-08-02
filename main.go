06468cbde85c3c7bd9e1d31f7f1a1672c7a775a1
wingram <mail@wingram.org>
2024-08-03 00:31:12 +0700

    chore: undo connect cloud sql

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/codegram01/wingram/config"
 	"github.com/codegram01/wingram/database"
 	"github.com/codegram01/wingram/server"
@@ -9,12 +11,12 @@ import (
 func main() {
 	cfg := config.Init()
 
-	// db, err := database.Connect(cfg)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	db, cleanup := database.ConnectGCloud()
-  	defer cleanup()
+	db, err := database.Connect(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// db, cleanup := database.ConnectGCloud()
+  	// defer cleanup()
 
 	sCfg := &server.ServerCfg{
 		Cfg: cfg,
